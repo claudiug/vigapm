4ceaa93939f35e66214deeeb6e3c498d8b0b42e8
claudiu <claudiu.garba@gmail.com>
2014-08-28 13:49:21 +0200

    should work in production also :/

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,16 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.mandrillapp.com',
+      port:                 587,
+      domain:               'vigap.com',
+      user_name:            'emy@vigap.com',
+      password:             '8lSLi3bWNFiJKoUsqFuDfg',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+  
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
