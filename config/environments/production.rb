ef2b9ffd9cc354b699be8ef83d8c0ca4a91eb983
claudiu <claudiu.garba@gmail.com>
2014-08-28 14:29:14 +0200

    remove ranking post

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
       password:             '8lSLi3bWNFiJKoUsqFuDfg',
       authentication:       'plain',
       enable_starttls_auto: true  }
-  
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
