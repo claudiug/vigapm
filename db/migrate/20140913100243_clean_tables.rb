a3bd20045773bdd608af4302678b9b6a57070e8c
claudiu <claudiu.garba@gmail.com>
2014-09-13 15:06:07 +0200

    again lets make dev server work :/

--- db/migrate/20140913100243_clean_tables.rb
+++ db/migrate/20140913100243_clean_tables.rb
@@ -1,8 +1,8 @@
 class CleanTables < ActiveRecord::Migration
   def change
-    drop_table :ahoy_events if table_exists?(:ahoy_events)
-    drop_table :event_notifications if table_exists?(:event_notifications)
-    drop_table :impressions if table_exists?(:impressions)
-    drop_table :visits if table_exists?(:visits)
+    # drop_table :ahoy_events if table_exists?(:ahoy_events)
+    # drop_table :event_notifications if table_exists?(:event_notifications)
+    # drop_table :impressions if table_exists?(:impressions)
+    # drop_table :visits if table_exists?(:visits)
   end
 end
