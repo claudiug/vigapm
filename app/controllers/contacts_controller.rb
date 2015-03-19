ecfbc24648d3d31f700d4b9d98b875492463c521
Gleb Pereyaslavsky <gleeeb@gmail.com>
2015-03-19 19:43:13 +0300

    Move contact form feedback parameter to a method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,11 +7,17 @@ class ContactsController < ApplicationController
   end
 
   def create
-    if simple_captcha_valid? && params[:contact][:feedback].present?
+    if simple_captcha_valid? && form_feedback.present?
       @sent = ContactMailer.feedback_received(current_user, params[:contact].delete(:feedback)).deliver
     end
 
-    @feedback_text = params[:contact][:feedback]
+    @feedback_text = form_feedback
     render :show
+  end
+
+  protected
+
+  def form_feedback
+    params[:contact][:feedback]
   end
 end
