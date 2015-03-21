6c63d69d4c3aaa6c22288a6010315cb56fb6b8e7
Timur Khafizov <hangmany@gmail.com>
2015-03-21 11:34:15 +0300

    Move comments tree render into partial

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def ranking_badge(user)
+    return content_tag(:span, class: "badge") unless user
+
+    span_class = "badge #{user.username.parameterize}-rank"
+    span_class << 'alert-danger' if user.ranking < 0
+
+    content_tag(:span, user.ranking, class: span_class)
+  end
 end
